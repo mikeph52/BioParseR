7baf23427ca3c10f17584ed1e485acef00bca6ab
mikeph52 <mikeph526@outlook.com>
2025-10-15 14:30:17 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+// BioParseR by mikeph_ 2025
+fn title(){
+    println!("BioParseR Pre-release, by mikeph_ 2025");
+}
+
+fn message(){
+    println!("This is a test");
+
+}
+
+fn main(){
+    title();
+    message();
+
 }
